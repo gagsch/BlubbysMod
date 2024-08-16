cabbaf19e22b0cdbf47b52650992333fcfb0d2d3
Gage Scheie <71233206+gagsch@users.noreply.github.com>
2024-08-16 04:21:59 -0400

    void log finally drops itself
    im going to bed

--- bmod/src/main/java/me/blubby/bmod/content/blocks/ModBlocks.java
+++ bmod/src/main/java/me/blubby/bmod/content/blocks/ModBlocks.java
@@ -1,17 +1,14 @@
 package me.blubby.bmod.content.blocks;
 
 import me.blubby.bmod.Blubby_sModOfDoom;
-import me.blubby.bmod.content.armor.BlubbyArmorItem;
-import me.blubby.bmod.content.armor.ModArmorMaterial;
 import me.blubby.bmod.content.blocks.custom.BurnablePillarBlock;
 import me.blubby.bmod.content.item.ModItems;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -25,7 +22,11 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS
             = DeferredRegister.create(ForgeRegistries.BLOCKS, Blubby_sModOfDoom.MOD_ID);
 
-    public static final RegistryObject<Block> VOID_LOG = registerBlock("void_log", () -> new BurnablePillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)), CreativeModeTab.TAB_MISC);
+    public static final RegistryObject<Block> VOID_LOG = registerBlock("void_log",
+            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
+                    .strength(2.0f)
+                    .sound(SoundType.WOOD)),
+            CreativeModeTab.TAB_MISC);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- bmod/src/main/java/me/blubby/bmod/content/blocks/custom/BurnablePillarBlock.java
+++ bmod/src/main/java/me/blubby/bmod/content/blocks/custom/BurnablePillarBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.RotatedPillarBlock;
